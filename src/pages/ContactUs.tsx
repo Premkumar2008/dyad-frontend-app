045c58cb99aaa726e93758ea87d1f48965020026
Premkumar2008 <premkumar200894ss@gmail.com>
2026-03-29 12:12:07 +0530

    API configure

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -701,11 +701,11 @@ const ContactUs: React.FC = () => {
               <h3 className="footer-column-title">Contact</h3>
               <div className="footer-contact-info">
                 <div className="contact-item">
-                  <span className="contact-symbol">✉</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-mail w-6 h-6 mr-3"><rect width="20" height="16" x="2" y="4" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></svg>
                   <span>info@dyadmd.com</span>
                 </div>
                 <div className="contact-item">
-                  <span className="contact-symbol">📍</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-map-pin w-6 h-6 mr-3"><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"></path><circle cx="12" cy="10" r="3"></circle></svg>
                   <span>2573 Pacific Coast Hwy, Ste A277 Torrance, CA 90505</span>
                 </div>
               </div>
